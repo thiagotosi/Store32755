f5a15bf8e2bd306d1247aaf8526bfdc3b7e91e12
thiagotosi <thiagotosi@gmail.com>
2017-10-11 10:39:53 -0300

    Sent by DeployFC at 11/10/2017 10:39:53

--- htm/js/autosuggesttbar.js
+++ htm/js/autosuggesttbar.js
@@ -4,6 +4,31 @@ var g2='5'; //Quantidade de itens do grupo g2(Páginas), Padrão 3, Máximo 9, 0 Nã
 var g3='5'; //Quantidade de itens do grupo g3(Produtos), Padrão 5, Máximo 9, 0 Não exibe
 var g4='5'; //Quantidade de itens do grupo g4(Notícias), Padrão 3, Máximo 9, 0 Não exibe
 
+var aGoogleTerms;
+
+function getGoogleTerms(sTerm){
+  "use strict";
+  aGoogleTerms=[];
+  FCLib$.fnGetSuggestions(sTerm,false,fnCallbackGetGoogleTerms);
+}
+
+function fnCallbackGetGoogleTerms(aTerms){
+  "use strict";
+  aGoogleTerms=aTerms;
+}
+
+function injectGoogleTerms(oSuggest,aTerms){
+  "use strict";
+  var iTerms=aTerms.length;
+  if(iTerms>0){
+    if(iTerms>5)iTerms=5;
+    if(!oSuggest["SearchTerms"])oSuggest["SearchTerms"]=[];
+    var iExistentes=oSuggest["SearchTerms"].length,aExistentes=[];
+    for(var i=0;i<iExistentes;i++)aExistentes[oSuggest.SearchTerms[i].label]=true;
+    for(var i=0;i<iTerms;i++)if(!aExistentes[aTerms[i]])oSuggest["SearchTerms"].push({"label":aTerms[i],"q":"?","category":"Buscas sugeridas"});
+  }
+}
+
 jQuery.noConflict();
 jQuery(function (jQuery) {
                   
@@ -43,141 +68,25 @@ jQuery("#autocomplete-toolbar").autocomplete({
     }
   },
   focus: function(event,ui) {
-      jQuery('input#autocomplete-toolbar').val(ui.item.label
-                .replace("&quot;",'"')
-                .replace("&apos;","'")
-                .replace("&amp;","&")
-                .replace("&lt;","<")
-                .replace("&gt;",">")
-                .replace("&#192;","À")
-                .replace("&#193;","Á")
-                .replace("&#194;","Â")
-                .replace("&#195;","Ã")
-                .replace("&#196;","Ä")
-                .replace("&#197;","Å")
-                .replace("&#198;","Æ")
-                .replace("&#199;","Ç")
-                .replace("&#200;","È")
-                .replace("&#201;","É")
-                .replace("&#202;","Ê")
-                .replace("&#203;","Ë")
-                .replace("&#204;","Ì")
-                .replace("&#205;","Í")
-                .replace("&#206;","Î")
-                .replace("&#207;","Ï")
-                .replace("&#208;","Ð")
-                .replace("&#209;","Ñ")
-                .replace("&#210;","Ò")
-                .replace("&#211;","Ó")
-                .replace("&#212;","Ô")
-                .replace("&#213;","Õ")
-                .replace("&#214;","Ö")
-                .replace("&#216;","Ø")
-                .replace("&#217;","Ù")
-                .replace("&#218;","Ú")
-                .replace("&#219;","Û")
-                .replace("&#220;","Ü")
-                .replace("&#221;","Ý")
-                .replace("&#222;","Þ")
-                .replace("&#223;","ß")
-                .replace("&#224;","à")
-                .replace("&#225;","á")
-                .replace("&#226;","â")
-                .replace("&#227;","ã")
-                .replace("&#228;","ä")
-                .replace("&#229;","å")
-                .replace("&#230;","æ")
-                .replace("&#231;","ç")
-                .replace("&#232;","è")
-                .replace("&#233;","é")
-                .replace("&#234;","ê")
-                .replace("&#235;","ë")
-                .replace("&#236;","ì")
-                .replace("&#237;","í")
-                .replace("&#238;","î")
-                .replace("&#239;","ï")
-                .replace("&#240;","ð")
-                .replace("&#241;","ñ")
-                .replace("&#242;","ò")
-                .replace("&#243;","ó")
-                .replace("&#244;","ô")
-                .replace("&#245;","õ")
-                .replace("&#246;","ö")
-                .replace("&#248;","ø")
-                .replace("&#249;","ù")
-                .replace("&#250;","ú")
-                .replace("&#251;","û")
-                .replace("&#252;","ü")
-                .replace("&#253;","ý")
-                .replace("&#254;","þ")
-                .replace("&#255;","ÿ")
-                );
+      var sValInputText = ui.item.label
+        .replace(/&quot;/g,'"')
+        .replace(/&apos;/g,"'")
+        .replace(/&amp;/g,"&")
+        .replace(/&lt;/g,"<")
+        .replace(/&gt;/g,">")
+        .replace(/&#(\d+);/g, function (m, n) { return String.fromCharCode(n); });
+      jQuery('input#autocomplete-toolbar').val(sValInputText);
       return false;
   },                                          
   source: function (request, response) {
+      getGoogleTerms(request.term);
       jQuery.ajax({
           url: "/autosuggest.asp?idloja="+ FC$.IDLoja +"&format=1&q="+ request.term +"&g1="+g1+"&g2="+g2+"&g3="+g3+"&g4="+g4,
           dataType: "json",
           type: "GET",                     
           success: function (data) {
-            var json0 = data.SearchTerms;
-            var json1 = data.Products;
-            var json2 = data.RelatedPages;
-            var json3 = data.News;
-            
-            
-            if(jQuery.isArray(json0)) {
-                json0 = json0;
-            }else{
-              json0 = [  ];
-            };                          
-            if(jQuery.isArray(json1)) {
-                json1 = json1;
-            }else{
-              json1 = [  ];
-            };                          
-            if(jQuery.isArray(json2)) {
-                json2 = json2;
-            }else{
-              json2 = [  ];
-            };                          
-            if(jQuery.isArray(json3)) {
-                json3 = json3;
-            }else{
-              json3 = [  ];
-            };
-                                                                  
-            var json = [].concat(json0,json1,json2,json3);
-            
-                                
-              //console.log(json);
-            
-            response(jQuery.map(json, function (item) {
-                return {
-                    category: item.category,
-                    //Termos
-                    t: item.label,
-                    q: item.q,
-                    //Produtos
-                    nm: item.label,
-                    id: item.id,
-                    c: item.c,
-                    im: item.im,
-                    op: item.op,
-                    fp: item.fp,
-                    v: item.v,
-                    //Paginas  
-                    p: item.label,
-                    u: item.u,
-                    //Noticias
-                    id: item.id,
-                    t:item.label,
-                    s: item.s,
-                    d: item.d,
-                    label: item.label
-                    }
-            }))                         
-
+            var iGoogleWait=0;
+            successAjax(request.term,data,response,iGoogleWait);
           },
           error: function (a, b, c) {
               debugger;
@@ -193,12 +102,13 @@ jQuery("#autocomplete-toolbar").autocomplete({
     if (item.q){      
       var sPlural = '';
       if (item.q >1){sPlural = 's';}else {sPlural = '&nbsp;&nbsp;';}
-      var t = item.label.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + jQuery.ui.autocomplete.escapeRegex(this.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>");           
+      var t = item.label.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + jQuery.ui.autocomplete.escapeRegex(this.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>");
+      var sQtdProduto=(item.q=="?")?"":"<span>" + item.q + "&nbsp;produto"+sPlural+"&nbsp;</span>";
       return jQuery("<li class='active'></li>")                
       .data("item.autocomplete", item)
-      .append("<a><table width='100%'><tr><td width='90%' id='as-nome-termos'><span>"+t+"</span></td><td align='right' id='as-qtd-termos'><span>"+item.q+"&nbsp;Produto"+sPlural+"&nbsp;</span></td></tr></table></a>")
+      .append("<a><table width='100%'><tr><td width='90%' id='as-nome-termos'><span>"+t+"</span></td><td align='right' id='as-qtd-termos'>"+ sQtdProduto +"</td></tr></table></a>")
       .appendTo(ul);                    
-    }
+    }  
 
     //Produtos
     if (item.c){
@@ -215,7 +125,7 @@ jQuery("#autocomplete-toolbar").autocomplete({
       var nm = item.label.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + jQuery.ui.autocomplete.escapeRegex(this.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>");
       return jQuery("<li class='active prods-itens'></li>")
       .data("item.autocomplete", item)
-      .append("<a><table width='100%'><tr height='75px'><td align=center class='SearchIMGFC'><span><img src='"+ FC$.PathPrd  + item.im + "' id='as-img-prod' style='vertical-align:middle;'></span></td><td width='55%'><div id='as-nomecat-prod'><span id='as-nome-prod'>" + nm + "</span><br/><span id='as-cat-prod'>" + item.c + "</span></div></td><td width=25 class='as-sale'>"+ sSale +"</td><td align='right' id='as-valor-prod' nowrap='nowrap'><span>" + valor +"</span></td></tr></table></a>")
+      .append("<a><table width='100%'><tr height='75px'><td align=center class='SearchIMGFC'><span><img src='"+ (item.im.indexOf('#')>=0?item.im.replace('#',FC$.PathPrdExt):FC$.PathPrd+item.im) +"' id='as-img-prod' style='vertical-align:middle;'></span></td><td width='55%'><div id='as-nomecat-prod'><span id='as-nome-prod'>" + nm + "</span><br/><span id='as-cat-prod'>" + item.c + "</span></div></td><td width=25 class='as-sale'>"+ sSale +"</td><td align='right' id='as-valor-prod' nowrap='nowrap'><span>" + valor +"</span></td></tr></table></a>")
       .appendTo(ul);
     }   
 
@@ -236,5 +146,60 @@ jQuery("#autocomplete-toolbar").autocomplete({
       .append("<a><table width='100%'><tr valign='0'><td width='65%' id='as-nome-not'>" + t + "</td><td align='right' id='as-data-not'>" + item.d + "</td></tr></table></a>")
       .appendTo(ul);                    
     }
+  }
+
+  function successAjax(sTerm,data,response,iGoogleWait){
+    if(!FCLib$.bDoneSuggestions && iGoogleWait<5){setTimeout(function(){successAjax(sTerm,data,response,++iGoogleWait);},100);return void(0);}
+    injectGoogleTerms(data,aGoogleTerms);
+    var json0 = data.SearchTerms;
+    var json1 = data.Products;
+    var json2 = data.RelatedPages;
+    var json3 = data.News;
+    if(jQuery.isArray(json0)) {
+        json0 = json0;
+    }else{
+      json0 = [  ];
+    };                          
+    if(jQuery.isArray(json1)) {
+        json1 = json1;
+    }else{
+      json1 = [  ];
+    };                          
+    if(jQuery.isArray(json2)) {
+        json2 = json2;
+    }else{
+      json2 = [  ];
+    };                          
+    if(jQuery.isArray(json3)) {
+        json3 = json3;
+    }else{
+      json3 = [  ];
+    };
+    var json = [].concat(json0,json1,json2,json3);
+    response(jQuery.map(json, function (item) {
+        return {
+            category: item.category,
+            //Termos
+            t: item.label,
+            q: item.q,
+            //Produtos
+            nm: item.label,
+            id: item.id,
+            c: item.c,
+            im: item.im,
+            op: item.op,
+            fp: item.fp,
+            v: item.v,
+            //Paginas  
+            p: item.label,
+            u: item.u,
+            //Noticias
+            id: item.id,
+            t:item.label,
+            s: item.s,
+            d: item.d,
+            label: item.label
+            }
+    }));
   }
 });
